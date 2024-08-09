567bde885715789b63894563cb462955de82ca2e
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-09 17:11:32 +0900

    vault backup: 2024-08-09 17:11:31

--- note/Go/pages/webapp/todo/app/models/todos.go
+++ note/Go/pages/webapp/todo/app/models/todos.go
@@ -13,11 +13,76 @@ type Todo struct {
 }
 
 func (u *User) CreateTodo(content string) (err error) {
-	cmd := `insert into todos (content, user_id, created_at) values (?, ?, ?)`
+	cmd := `insert into todos (
+		content,
+		user_id,
+		created_at) values (?, ?, ?)`
 
-	_, err = Db.Exec(cmd, content, u.ID, time.now())
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
 	if err != nil {
 		log.Fatalln(err)
 	}
 	return err
+}
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+		where id = ?`
+	todo = Todo{}
+
+	err = Db.QueryRow(cmd, id).Scan(
+		&todo.ID,
+		&todo.Content,
+		&todo.UserID,
+		&todo.CreatedAt,
+	)
+	return todo, err
+}
+
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt,
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+}
+
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+		where user_id = ?`
+
+		rows, err := Db.Query((cmd, u.ID))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for rows.Next() {
+			var todo Todocmd
+			err = rows.Scan(
+				&todo.ID,
+				&todo.Content,
+				&todo.UserID,
+				&todo.CreatedAt,
+			)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			todos = append(todos, todo)
+		}
+		rows.Close()
 }
