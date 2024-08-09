ca82d463d5c8c2f4c1f7dd5af53d1f90163fcc93
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-09 23:07:04 +0900

    vault backup: 2024-08-09 23:07:03

--- note/Go/pages/webapp/todo/app/controllers/server.go
+++ note/Go/pages/webapp/todo/app/controllers/server.go
@@ -17,7 +17,9 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
+
 	http.Handle("/static", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
