4f16ffa694668797df97a91c217165b8df1ecc58
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-09 20:59:21 +0900

    vault backup: 2024-08-09 20:59:20

--- note/Go/pages/webapp/todo/app/controllers/server.go
+++ note/Go/pages/webapp/todo/app/controllers/server.go
@@ -3,6 +3,8 @@ package controllers
 import "net/http"
 
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
--- note/Go/pages/webapp/todo/config/config.go
+++ note/Go/pages/webapp/todo/config/config.go
@@ -1,38 +1,40 @@
 package config
 
 import (
-  "log"
-  "todo_app/utils"
+	"log"
+	"todo_app/utils"
 
-  "gopkg.in/gi-ini/ini.v1"
+	"gopkg.in/gi-ini/ini.v1"
 )
 
 type ConfigList struct {
-  Port      string
-  SQLDriver string
-  DbName    string
-  LogFile   string
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+	Static    string
 }
 
 // globalに宣言して外部ファイルから呼び出せるように
 var Config ConfigList
 
 func init() {
-  LoadConfig()
-  utils.LoggingSettings(Config.LogFile)
+	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
-  // iniを読み込む
-  cfg, err := ini.Load("config.ini")
-  if err != nil {
-    log.Fatalln(err)
-  }
-  // iniの内容をConfigListに格納
-  Config = ConfigList{
-    Port:      cfg.Section("web").Key("port").MustString("8080"),
-    SQLDriver: cfg.Section("db").Key("driver").String(),
-    DbName:    cfg.Section("db").Key("name").String(),
-    LogFile:   cfg.Section("web").Key("logfile").String(),
-  }
+	// iniを読み込む
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// iniの内容をConfigListに格納
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
+	}
 }
