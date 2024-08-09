c16fdf9e4814d75f3f07f17c72ec1ac7dae95b98
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-09 14:11:41 +0900

    vault backup: 2024-08-09 14:11:40

--- note/Go/pages/webapp/todo/app/models/base.go
+++ note/Go/pages/webapp/todo/app/models/base.go
@@ -1,5 +1,33 @@
 package models
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"todo_app/config"
+
 	_ "gitbub.com/mattn/go-sqlite3"
 )
+
+var Db *sql.DB
+
+var err error
+
+const (
+	tableNameUser = "users"
+)
+
+func init() {
+	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		uuid STRING NOT NULL UNIQUE,
+		name STRING,
+		email STRING,
+		password STRING,
+		create_at DATATIME)`, tableNameUser)
+}
