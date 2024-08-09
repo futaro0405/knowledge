5b516d20e6b2579fa734d9c6c241041dee0bf15c
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-09 18:01:44 +0900

    vault backup: 2024-08-09 18:01:43

--- note/Go/pages/webapp/todo/main.go
+++ note/Go/pages/webapp/todo/main.go
@@ -5,13 +5,37 @@ import (
 )
 
 func main() {
-	fmt.Pritnln(models.Db)
+	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
+	/*
+		u := &models.User{}
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.PassWord = "testtest"
+		u.CreateUser()
+	*/
 
-	// u.CreateUser()
+	/*
+		u, _ := models.GetUser(l)
+		u.Name = "Test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+	*/
+
+	/*
+		u, _ = models.GetUser(l)
+		u.DeleteUser()
+	*/
+
+	/*
+		user, _ := models.GetUser(3)
+		user.CreateTodo("First Todo")
+	*/
+
+	/*
+		todos , _ = models.GetTodos()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
 }
