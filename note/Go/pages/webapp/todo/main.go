023ad3029c4464cb35c71e56d82c3f46a7e523d8
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-11 13:55:45 +0900

    vault backup: 2024-08-11 13:55:44

--- note/Go/pages/webapp/todo/main.go
+++ note/Go/pages/webapp/todo/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println(models.Db)
 	user := models.GetUserByEmail("test@example.com")
+	fmt.Println(user)
+
+	session, err := user.CreateSession()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(session)
 }
