87c0291ff88e8b93dc57e92303677886ab020a71
futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-13 09:48:37 +0900

    vault backup: 2024-08-13 09:48:37

--- note/Go/pages/webapp/todo/app/controllers/route_main.go
+++ note/Go/pages/webapp/todo/app/controllers/route_main.go
@@ -5,6 +5,12 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
 	generateHTML(w, "Hello", "layout", "public_navbar", "top")
 }
 
@@ -13,6 +19,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Redirect(w, r, "/", 302)
 	} else {
-		generateHTML(w, nil, "layout", "private", "index")
+		generateHTML(w, nil, "layout", "private_navbar", "index")
 	}
 }
--- note/Go/pages/webapp/todo/app/controllers/server.go
+++ note/Go/pages/webapp/todo/app/controllers/server.go
@@ -15,17 +15,6 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 	templates.ExecuteTemplate(w, "layout", data)
 }
 
-func StartMainServer() error {
-	files := http.FileServer(http.Dir(config.Config.Static))
-
-	http.Handle("/static", http.StripPrefix("/static/", files))
-	http.HandleFunc("/", top)
-	http.HandleFunc("/signup", signup)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/authenticate", authenticate)
-	return http.ListenAndServe(":"+config.Config.Port, nil)
-}
-
 func sessin(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
 	cookie, err := r,Cookie("_cookie")
 	if err == nil {
@@ -35,4 +24,16 @@ func sessin(w http.ResponseWriter, r *http.Request) (sess models.Session, err er
 		}
 	}
 	return sess, err
+}
+
+func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+
+	http.Handle("/static", http.StripPrefix("/static/", files))
+	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
+	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
