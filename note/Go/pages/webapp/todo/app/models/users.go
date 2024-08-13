e7a8cbd17a0c85a3b3bdb93d05afc3701d76f88b
futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-13 10:34:04 +0900

    vault backup: 2024-08-13 10:34:04

--- note/Go/pages/webapp/todo/app/models/users.go
+++ note/Go/pages/webapp/todo/app/models/users.go
@@ -135,3 +135,12 @@ func (sess *Session) CheckSession() (valid bool, err error) {
 	}
 	return valid, err
 }
+
+func (sess *Session) DeleteSessionByUUID() (err error) {
+	cmd := `delete from sessions where uuid = ?`
+	_, err = DB.Exec(cmd, sess.UUID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
