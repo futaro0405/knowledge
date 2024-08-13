2cfea522752c89894b1a8fd63b1191f75c5df4f0
futaro0405 <fkswtkm0405.web@gmail.com>
2024-08-13 14:54:04 +0900

    vault backup: 2024-08-13 14:54:04

--- note/Go/pages/webapp/todo/app/controllers/route_main.go
+++ note/Go/pages/webapp/todo/app/controllers/route_main.go
@@ -59,3 +59,20 @@ func todoSave(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/todos", 302)
 	}
 }
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		generateHTML(w, t, "layout", "private_nav")
+	}
+}
