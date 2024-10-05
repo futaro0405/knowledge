ccc78320f03da982d3b4fc2717d78f42b4741435
futaro0405 <fkswtkm0405@gmail.com>
2024-10-05 17:48:13 +0900

    vault backup: 2024-10-05 17:48:13

--- note/Go/restful_api/exe/main.go
+++ note/Go/restful_api/exe/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 type Article struct {
-	Id		string `json:Id`
-	Title	string `json:Title`
-	Desc	string `json:desc`
-	Content string `json:content`
+    Id      string `json:"Id"`
+    Title   string `json:"Title"`
+    Desc    string `json:"desc"`
+    Content string `json:"content"`
 }
 
 var Articles []Article
@@ -33,6 +33,44 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+     reqBody, _ := ioutil.ReadAll(r.Body)
+    var article Article 
+    json.Unmarshal(reqBody, &article)
+ 
+    Articles = append(Articles, article)
+
+    json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func updateArticle(w http.ResponseWriter, r http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var updatedEvent Article
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &updatedEvent)
+	for i, article := range Articles {
+		if article.Id == id {
+			article.Title = updatedEvent.Title
+			article.Desc = updatedEvent.Desc
+			article.Content = updatedEvent.Content
+			Articles[i] = article
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the homePage")
 	fmt.Println("Endpoint Hit: homePage")
@@ -43,6 +81,9 @@ func handleRequests() {
 
     myRouter.HandleFunc("/", homePage)
     myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
     log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
